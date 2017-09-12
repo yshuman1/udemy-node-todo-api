a3c5f498eb4ddfa421f966ada4c5cbe5a7a62f94
yshuman1 <yshuman@loyalloops.com>
2017-09-12 16:27:56 -0700

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started up at port ${port}``);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
