9eddec3f665d5c247397c16c2b16236846627123
yshuman1 <yshuman@loyalloops.com>
2017-09-13 20:46:39 -0700

    fixed path for db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
-//mongoose.connect('mongodb://webapp:pword@ds135534.mlab.com:35534/udemy-node-course');
+//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://webapp:pword@ds135534.mlab.com:35534/udemy-node-course');
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
