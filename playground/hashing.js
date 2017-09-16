788db09e97b94d188d34138e76c5544ac20eef3d
yshuman1 <yshuman@loyalloops.com>
2017-09-16 01:30:10 -0700

    hash user pw before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,36 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
-//
-// var message = 'I am user number 3';
-// var hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
-//
-//
-// var data = {
-//   id: 4
-// };
-//
-// var token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
-//
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if (resultHash === token.hash) {
-//   console.log('data wasnt changed');
-// } else {
-//   console.log('data was changed dont trust!');
-// }
+var hashedPassword = '$2a$10$MjQ2BiE00iiE3rhBK6Arz.lNj5hnKgZpcYUsuNj8mCfcXeah9a.Zi';
+
+bcrypt.compare('123', hashedPassword, (err,res) => {
+  console.log(res);
+});
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
